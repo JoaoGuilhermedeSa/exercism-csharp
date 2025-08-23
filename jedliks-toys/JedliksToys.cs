bba45cd7be1179d6d95e65c6ebb42ff80d3d2968
João Guilherme de Sá <jaum_ipm@hotmail.com>
2025-08-23 14:52:01 -0300

    jedliks-toys

---
+++ jedliks-toys/JedliksToys.cs
@@ -0,0 +1,32 @@
+class RemoteControlCar{
+
+    private int _distanceDriven = 0;
+    private int _battery = 100;
+    public static RemoteControlCar Buy()
+    {
+        return new RemoteControlCar();
+    }
+
+    public string DistanceDisplay()
+    {
+        return "Driven " +_distanceDriven + " meters";
+    }
+
+    public string BatteryDisplay()
+    {
+        if (_battery == 0)
+        {
+            return "Battery empty";
+        }
+        return "Battery at " +_battery+ "%";
+    }
+
+    public void Drive()
+    {
+        if (_battery > 0)
+        {
+            _distanceDriven += 20;
+            _battery -= 1;
+        }
+    }
+}
